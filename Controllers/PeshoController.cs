ef2316f8fc021d6ad71f8fe3f57fe0b0bde45ef8
VelinP <dottallot1@hotmail.com>
2024-04-14 01:11:14 +0300

    Made a baseline project

---
+++ Controllers/PeshoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Controllers
+{
+    public class PeshoController : Controller
+    {
+        public IActionResult PeshoHtml()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int BaseId { get; set; }
+
+        [Required]
+        public string BaseUsername { get; set; }
+        [Required]
+        public int Age { get; set; }
+
+    }
+}
